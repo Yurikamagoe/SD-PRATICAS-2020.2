638ef9c659133ee59cf6db70748a3b35f99ee455
Matheus Romero <matheus.hoshigaki@hotmail.com>
2021-03-21 20:05:28 -0300

    #2 - Mudanças no RMi

--- crateus/src/main/java/com/example/crateus/controller/RestClient.java
+++ crateus/src/main/java/com/example/crateus/controller/RestClient.java
@@ -14,6 +14,7 @@ public class RestClient {
 	
 	private static final String CREATE = "http://localhost:8080/api/people";
 	private static final String GET_PERSON_BY_ID= "http://localhost:8080/api/people/{id}";
+	private static final String SEND_MESSAGE= "rmi://127.0.0.1:32001/Message";
 	
 	static RestTemplate rest = new RestTemplate();
 
@@ -29,6 +30,11 @@ public class RestClient {
 		Person person = rest.getForObject(GET_PERSON_BY_ID, Person.class, param);
 		
 		return person;
+	}
+	
+	public static void SendMessage(String message) {
+		ResponseEntity<String> p = rest.postForEntity(SEND_MESSAGE, message, String.class);
+		System.out.println(p.getBody());
 	}
 	
 	
